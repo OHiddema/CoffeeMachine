e009f4f11065cae0a08cbf82936e6eecc775cdd5
OHiddema <oegehiddema@gmail.com>
2022-10-28 19:28:50 +0200

    second commit

--- Coffee Machine/task/src/machine/Main.java
+++ Coffee Machine/task/src/machine/Main.java
@@ -7,13 +7,12 @@ public class Main {
         CoffeeMachine cofMac = new CoffeeMachine(400, 540, 120, 9, 550);
         Scanner scanner = new Scanner(System.in);
 
-        boolean exit = false;
-        while (!exit) {
-            switch (cofMac.getState()) {
+        State currentState = cofMac.getState();
+        do {
+            switch (currentState) {
                 case MAIN_MENU -> {
                     System.out.println("Write action (buy, fill, take, remaining, exit): ");
-                    String option = scanner.nextLine();
-                    cofMac.processInput(option);
+                    cofMac.processInput(scanner.nextLine());
                 }
                 case FILL_STEP1 -> {
                     System.out.println("Write how many ml of water you want to add:");
@@ -35,8 +34,8 @@ public class Main {
                     System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
                     cofMac.processInput(scanner.nextLine());
                 }
-                case EXIT -> exit = true;
             }
-        }
+            currentState = cofMac.getState();
+        } while (currentState != State.EXIT);
     }
 }
